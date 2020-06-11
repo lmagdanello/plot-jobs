d7f526948bc41304f43eca7b79799bb78a89f72f
Leonardo Magdanello Araujo <47561599+lmagdanello@users.noreply.github.com>
2020-06-11 00:57:17 -0300

    plot-jobs.py

---
+++ plot-jobs/plot-jobs.py
@@ -0,0 +1,66 @@
+import subprocess
+import matplotlib.pyplot as plt
+import argparse
+import sys
+
+def plot_jobs(date):
+
+        # vars_j: Utiliza o slurm accounting para capturar os estados dos Jobs a partir da data informada pelo usuário
+
+        failed_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'FAILED\' | wc -l'.format(date))
+        complete_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'COMPLETED\' | wc -l'.format(date))
+        running_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'RUNNING\' | wc -l'.format(date))
+        cancelled_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'CANCELLED\' | wc -l'.format(date))
+
+        # state_dict: Armazena os estados dos Jobs em um dicionario
+
+        state_dict = { 'completed': int(complete_j), 'failed': int(failed_j), 'running': int(running_j), 'cancelled': int(cancelled_j) }
+
+        # sorted_dict: Mapeia o dicionario de estados de forma crescente
+
+        sorted_dict = {k: state_dict[k] for k in sorted(state_dict, key=state_dict.get)}
+
+        '''
+         Definimos (x,y):
+                - X sao os estados dos Jobs e as chaves do dicionario
+                - Y sao os valores contabilizados para cada estado e os valores de cada chave no dicionario
+        '''
+
+        states = list(sorted_dict.keys())
+        values = list(sorted_dict.values())
+
+        # Realizamos a criacao do graph
+        plt.figure(figsize=(25,25))
+        plt.subplot(221)
+        #explode = (0.02, 0.02, 0.02, 0.02)
+
+        plt.pie(values, shadow=False, startangle=90, radius=1800)
+        title = 'Job Status since: "{}"'.format(date)
+
+        # Criamos a box de legendas
+        plt.legend(
+            loc='center left',
+            labels=['%s: %1.1f%%' % (
+                l, (float(s) / sum(values)) * 100) for l, s in zip(states, values)], 
+            title=''.join('{:^50}'.format(s) for s in title.split('\n')), 
+            bbox_to_anchor=(1, 0, 0, 1.3)
+        )
+        plt.axis('equal')
+        plt.tight_layout()
+
+        # Salvamos a figura no dir presente, com o sufixo da data utilizada como parametro
+
+        plt.savefig('./pie-plot-{}.png'.format(date))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--date',
+            type=plot_jobs,
+            action='store',
+            help='Starting time, format: 2020-02-02')
+
+date = parser.parse_args()
+
+if len(sys.argv) <= 1:
+    sys.argv.append('--help')
+    options = parser.parse_args()
+    options.func()
