2c9589f706749d33071bc49faf08d799956b2c61
Leonardo Magdanello Araujo <47561599+lmagdanello@users.noreply.github.com>
2020-07-11 20:06:14 -0300

    Update plot-jobs.py
    Changes:
    - created the function for the parser;
    - changed the plot so that the user can add the period for analysis;
    - identified with red the status of the jobs that failed;
    - added explode to the status of failed jobs;
    - changed the legend to present the analyzed period.

--- plot-jobs/plot-jobs.py
+++ plot-jobs/plot-jobs.py
@@ -2,64 +2,68 @@ import subprocess
 import matplotlib.pyplot as plt
 import argparse
 import sys
+import os
+from datetime import date
 
-def plot_jobs(date):
+def plot_jobs(starttime,endtime):
+    """
+    Plot_jobs is responsible for collecting data (using slurm accounting) from completed, failed and canceled jobs, mapping them in a dictionary and creating the pie chart
+    """
 
-        # vars_j: Utiliza o slurm accounting para capturar os estados dos Jobs a partir da data informada pelo usuário
+    failed_j = subprocess.getoutput('sudo sacct --starttime {} --endtime {} -X | egrep -E \'FAILED\' | wc -l'.format(starttime, endtime))
+    complete_j = subprocess.getoutput('sudo sacct --starttime {} --endtime {} -X | egrep -E \'COMPLETED\' | wc -l'.format(starttime, endtime))
+    cancelled_j = subprocess.getoutput('sudo sacct --starttime {} --endtime {} -X | egrep -E \'CANCELLED\' | wc -l'.format(starttime, endtime))
 
-        failed_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'FAILED\' | wc -l'.format(date))
-        complete_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'COMPLETED\' | wc -l'.format(date))
-        cancelled_j = subprocess.getoutput('sudo sacct --starttime {} | egrep -E \'CANCELLED\' | wc -l'.format(date))
+    state_dict = { 'completed': int(complete_j), 'failed': int(failed_j), 'cancelled': int(cancelled_j) }
 
-        # state_dict: Armazena os estados dos Jobs em um dicionario
+    states = list(state_dict.keys())
+    values = list(state_dict.values()) 
+	
+    colors = ('green', 'red', 'grey')
+    plt.figure(figsize=(10,10))
+    explode = (0, 0.1, 0)
+    plt.pie(values, explode=explode, shadow=True, wedgeprops={'linewidth' : 1, 'edgecolor' : 'black'}, colors=colors, autopct='%8.1f%%')
+    title = 'Job Status from {} to {} '.format(starttime, endtime)
 
-        state_dict = { 'completed': int(complete_j), 'failed': int(failed_j), 'cancelled': int(cancelled_j) }
+    plt.legend(
+        loc='center left',
+        labels=['%s: %1.1f%%' % (
+                 s, (float(v) / sum(values)) * 100) for s, v in zip(states, values)], 
+                 title=''.join('{:^50}'.format(s) for s in title.split('\n')), 
+                 bbox_to_anchor=(1, 0, 0, 1.3)
+               )
+    plt.axis('equal')
+    plt.tight_layout()
 
-        # sorted_dict: Mapeia o dicionario de estados de forma crescente
+    '''
+    Saves the file as: pie-plot-{OS.HOSTNAME}-{CURRENT DATE}.png in the current directory
+    '''
+    host = os.uname()[1]
+    today = date.today()
+    d = today.strftime("%y-%m-%d")
+    plt.savefig('./pie-plot-{0}-{1}.png'.format(host, d))
 
-        sorted_dict = {k: state_dict[k] for k in sorted(state_dict, key=state_dict.get)}
+def parser():
+    """
+    Parser is responsible for receiving the arguments for the plot (start time and end time)
+    """
 
-        '''
-         Definimos (x,y):
-                - X sao os estados dos Jobs e as chaves do dicionario
-                - Y sao os valores contabilizados para cada estado e os valores de cada chave no dicionario
-        '''
-
-        states = list(sorted_dict.keys())
-        values = list(sorted_dict.values()) 
-
-        # Realizamos a criacao do graph
-        colors = ('red', 'grey', 'green')
-        plt.figure(figsize=(10,10))
-        explode = (0, 0, 0.1)
-
-        plt.pie(values, explode=explode, shadow=True, wedgeprops={'linewidth' : 1, 'edgecolor' : 'black'}, colors=colors, autopct='%8.1f%%')
-        title = 'Job Status since: "{}"'.format(date)
-
-        # Criamos a box de legendas
-        plt.legend(
-            loc='center left',
-            labels=['%s: %1.1f%%' % (
-                s, (float(v) / sum(values)) * 100) for s, v in zip(states, values)], 
-            title=''.join('{:^50}'.format(s) for s in title.split('\n')), 
-            bbox_to_anchor=(1, 0, 0, 1.3)
-        )
-        plt.axis('equal')
-        plt.tight_layout()
-
-        # Salvamos a figura no dir presente, com o sufixo da data utilizada como parametro
-
-        plt.savefig('./pie-plot-{}.png'.format(date))
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--date',
-            type=plot_jobs,
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--starttime',
             action='store',
             help='Starting time, format: 2020-02-02')
 
-date = parser.parse_args()
+    parser.add_argument('-e', '--endtime',
+            action='store',
+            help='Starting time, format: 2020-02-02')
+    args = vars(parser.parse_args())
 
-if len(sys.argv) <= 1:
-    sys.argv.append('--help')
-    options = parser.parse_args()
-    options.func()
+    if len(sys.argv) <= 1:
+       sys.argv.append('--help')
+       options = parser.parse_args()
+       options.func()
+
+    plot_jobs((args['starttime']), (args['endtime']))
+
+if __name__ == "__main__":
+	parser()
